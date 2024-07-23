cbdc2b2f903e90587af7511548f87429a9ccc1a1
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2024-07-23 22:46:56 +0530

    fix: onboarding bug fixes

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/db"
 import { GroupSelectorClient } from "@/components/GroupSelectorClient";
+import { permanentRedirect } from 'next/navigation'
 
 export default async function OnboardingPage() {
 	const {userId} = auth()
@@ -8,6 +9,15 @@ export default async function OnboardingPage() {
 		return <h1>Login first</h1>
 	}
 	
+	const userData = await prisma.user.findFirst({
+		where: {
+			id: userId
+		}
+	})
+
+	if(!(userData?.currentGroup === "unknown")){
+		permanentRedirect("/")
+	}
 	
 	const data = await prisma.class_group.findMany();
 	return (
