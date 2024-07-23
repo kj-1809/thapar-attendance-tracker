ed9275eb62e5b9a16481a1b3c6e59aa9288c5318
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2024-07-23 19:08:54 +0530

    fix: limit the dates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import ProfileDropdown from "@/components/ProfileDropdown";
 import Link from "next/link";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 export const Navbar = () => {
+	
+
 	return (
 		<div className="h-16 bg-purple-200 sticky w-full overflow-hidden top-0 flex justify-between items-center px-2">
 			<Link href="/">
