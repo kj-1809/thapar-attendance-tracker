158960cc9e7d9f0ddfbe14d35a6b67d41bb5d066
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2024-08-01 10:15:02 +0530

    fix: UI

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react";
 export default function LoadingHome() {
   return (
     <div className="flex justify-center mt-10">
-      <LoaderCircle className="animate-spin" size = {32}/>;
+      <LoaderCircle className="animate-spin" size = {32}/>
     </div>
   );
 }
